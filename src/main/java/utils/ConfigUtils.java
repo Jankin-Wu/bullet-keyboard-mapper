eaebc283825d4f682b4b459ed7282d457b4557c3
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2024-02-25 13:47:04 +0800

    init

---
+++ src/main/java/utils/ConfigUtils.java
@@ -0,0 +1,64 @@
+package utils;
+
+import config.BasicConfig;
+import org.yaml.snakeyaml.Yaml;
+
+import java.io.InputStream;
+import java.util.Map;
+
+/**
+ * @author jankinwu
+ * @description 配置解析工具类
+ * @date 2024/2/25 13:19
+ */
+public class ConfigUtils {
+
+    public static <T> T getConfig(String fileName, Class<T> entityClass) {
+        try {
+            InputStream inputStream = ConfigUtils.class.getClassLoader().getResourceAsStream(fileName);
+            if (inputStream != null) {
+                Yaml yaml = new Yaml();
+                Map<String, Object> data = yaml.load(inputStream);
+                return yamlToObject(data, entityClass);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static <T> T yamlToObject(Map<String, Object> data, Class<T> entityClass) {
+        try {
+            T instance = entityClass.getDeclaredConstructor().newInstance();
+            for (Map.Entry<String, Object> entry : data.entrySet()) {
+                String fieldName = entry.getKey();
+                Object fieldValue = entry.getValue();
+                setFieldValue(instance, fieldName, fieldValue);
+            }
+            return instance;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static <T> void setFieldValue(T instance, String fieldName, Object fieldValue) {
+        try {
+            java.lang.reflect.Field field = instance.getClass().getDeclaredField(fieldName);
+            field.setAccessible(true);
+            field.set(instance, fieldValue);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        // 示例：将配置映射到实体类
+        BasicConfig config = ConfigUtils.getConfig("config.yml", BasicConfig.class);
+        if (config != null) {
+            System.out.println("Name: " + config.getAppId());
+            System.out.println("Version: " + config.getIdCode());
+            // 可以进一步处理其他属性
+        }
+    }
+}
