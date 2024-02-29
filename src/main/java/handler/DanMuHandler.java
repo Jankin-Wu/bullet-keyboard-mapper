ed4374870fef1380d6b00213c6e80a7aae0685d1
Jankin-Wu <63941607+Jankin-Wu@users.noreply.github.com>
2024-02-29 23:18:48 +0800

    feat: 支持更多快捷键映射

--- src/main/java/handler/DanMuHandler.java
+++ src/main/java/handler/DanMuHandler.java
@@ -1,16 +1,23 @@
 package handler;
 
+import cn.hutool.core.collection.CollUtil;
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.StrUtil;
 import com.alibaba.fastjson2.JSON;
 import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
-import lombok.extern.slf4j.Slf4j;
+import enums.KeyMappingEnum;
 import utils.KeyboardSimulationUtils;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
+import java.util.Objects;
 
 
 /**
@@ -18,11 +25,12 @@ import java.util.Map;
  * @description 弹幕处理器
  * @date 2024/2/26 23:34
  */
-@Slf4j
 public class DanMuHandler {
 
-    private Map<String, Character> map;
+    private final Map<String, String> map = new HashMap<>();
     private final String FILE_NAME = "keyMapping.json";
+    private final String EXTERNAL_FILE_PATH = "./keyMapping.json";
+
 
     public void handleDanMu(String content) {
         initMap();
@@ -30,34 +38,65 @@ public class DanMuHandler {
         JSONObject data = contentJson.getJSONObject("data");
         String msg = data.getString("msg");
         String uname = data.getString("uname");
-        log.info("[弹幕] {}：{}", uname, msg);
-        map.forEach((key, value) -> {
-            if (key.equals(msg)) {
-                KeyboardSimulationUtils.pressAndRelease(value);
+        System.out.println(DateUtil.now() + " [弹幕] " + uname + "：" + msg);
+        String key = map.get(msg.toUpperCase(Locale.ROOT));
+        if (StrUtil.isNotBlank(key)) {
+            Integer eventCode = KeyMappingEnum.getEventCode(key);
+            if (Objects.nonNull(eventCode)) {
+                System.out.println(DateUtil.now() + " 模拟按键：" + key);
+                KeyboardSimulationUtils.pressAndRelease(eventCode);
             }
-        });
+        }
     }
 
-    private void initMap() {
-        try (InputStream inputStream = DanMuHandler.class.getClassLoader().getResourceAsStream(FILE_NAME)) {
-            if (inputStream != null) {
-                byte[] bytes = inputStream.readAllBytes();
-                String jsonString = new String(bytes, StandardCharsets.UTF_8);
-
-                JSONArray jsonArray = JSON.parseArray(jsonString);
-                for (int i = 0; i < jsonArray.size(); i++) {
-                    JSONObject jsonObject = jsonArray.getJSONObject(i);
-                    String msg = jsonObject.getString("msg");
-                    String key = jsonObject.getString("key");
-                    if (msg != null && key != null && key.length() == 1) {
-                        map.put(msg, key.charAt(0));
-                    }
+    public void initMap() {
+        if (CollUtil.isNotEmpty(map)) {
+            return;
+        }
+        // 使用双检锁避免高并发时重复初始化map
+        synchronized (this) {
+            if (CollUtil.isNotEmpty(map)) {
+                return;
+            }
+            File externalFile = new File(EXTERNAL_FILE_PATH);
+            // 检查外部目录是否存在keyMapping.json文件，如果有就使用外部的映射文件
+            if (externalFile.exists()) {
+                try (InputStream inputStream = new FileInputStream(EXTERNAL_FILE_PATH)) {
+                    readJson(inputStream);
+                } catch (Exception e) {
+                    System.out.println(DateUtil.now() + " 映射文件解析异常");
+                    e.printStackTrace();
                 }
             } else {
-                System.out.println("File not found: " + FILE_NAME);
+                System.out.println("检测不到外部映射文件，开始加载内部默认映射文件。。。");
+                try (InputStream inputStream = DanMuHandler.class.getClassLoader().getResourceAsStream(FILE_NAME)) {
+                    if (Objects.nonNull(inputStream)) {
+                        readJson(inputStream);
+                    }
+                } catch (Exception e) {
+                    System.out.println(DateUtil.now() + " 找不到映射文件");
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+    }
+
+    private void readJson(InputStream inputStream) throws IOException {
+        System.out.println(DateUtil.now() + " 加载按键映射...");
+        byte[] bytes = inputStream.readAllBytes();
+        String jsonString = new String(bytes, StandardCharsets.UTF_8);
+
+        JSONArray jsonArray = JSON.parseArray(jsonString);
+        if (jsonArray.size() <= 0) {
+            System.out.println(DateUtil.now() + " 按键映射读取失败");
+        }
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONObject jsonObject = jsonArray.getJSONObject(i);
+            String msg = jsonObject.getString("msg");
+            String key = jsonObject.getString("key");
+            if (StrUtil.isNotBlank(msg) && StrUtil.isNotBlank(key)) {
+                map.put(msg, key);
+            }
+        }
+        System.out.println(DateUtil.now() + " 按键映射加载完毕");
     }
 }
